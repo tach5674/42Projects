df367c06d87a44bbba3e0d24ea18453d744df958
Mikayel Zohrabyan <mzohraby@c2r11s4.42yerevan.am>
2025-04-08 18:48:19 +0400

    das

--- 42Cursus/philo/philosophers.c
+++ 42Cursus/philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: mzohraby <mzohraby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 14:12:55 by mzohraby          #+#    #+#             */
-/*   Updated: 2025/04/08 18:15:22 by mzohraby         ###   ########.fr       */
+/*   Updated: 2025/04/08 18:41:08 by mzohraby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,16 +38,15 @@ void	philosopher_odd(t_philo *self)
 		pthread_mutex_lock(self->right);
 		print_msg(" has taken a fork\n", self);
 		print_msg(" is eating\n", self);
+		usleep(self->table->time_to_eat);
 		pthread_mutex_lock(&self->table->meal_mutex);
 		self->last_meal_time = get_time();
 		pthread_mutex_unlock(&self->table->meal_mutex);
-		usleep(self->table->time_to_eat);
 		pthread_mutex_unlock(self->left);
 		pthread_mutex_unlock(self->right);
 		print_msg(" is sleeping\n", self);
 		usleep(self->table->time_to_sleep);
 		print_msg(" is thinking\n", self);
-		usleep(self->table->time_to_sleep);
 		pthread_mutex_lock(&self->table->simulation_mutex);
 		if (self->table->simulation_over)
 		{
@@ -67,16 +66,15 @@ void	philosopher_even(t_philo *self)
 		pthread_mutex_lock(self->left);
 		print_msg(" has taken a fork\n", self);
 		print_msg(" is eating\n", self);
+		usleep(self->table->time_to_eat);
 		pthread_mutex_lock(&self->table->meal_mutex);
 		self->last_meal_time = get_time();
 		pthread_mutex_unlock(&self->table->meal_mutex);
-		usleep(self->table->time_to_eat);
 		pthread_mutex_unlock(self->left);
 		pthread_mutex_unlock(self->right);
 		print_msg(" is sleeping\n", self);
 		usleep(self->table->time_to_sleep);
 		print_msg(" is thinking\n", self);
-		usleep(self->table->time_to_sleep);
 		pthread_mutex_lock(&self->table->simulation_mutex);
 		if (self->table->simulation_over)
 		{
@@ -93,7 +91,7 @@ void	*philosopher_thread(void *philo)
 	
 	self = philo;
 	
-	if (self->id % 2)
+	if (self->id % 2)	
 		philosopher_odd(self);
 	else
 		philosopher_even(self);
@@ -159,6 +157,19 @@ int	simulation_start(t_table *table)
 		}	
 		i++;
 	}
+	// i = 0;
+	// while (i < table->n)
+	// {
+	// 	table->start_time = get_time();
+	// 	table->philos->last_meal_time = get_time();
+	// 	if (!(i % 2) && pthread_create(&(table->philos[i].thread), NULL, philosopher_thread, &table->philos[i]))
+	// 	{
+	// 		free(table->forks);
+	// 		free(table->philos);
+	// 		return (0);
+	// 	}	
+	// 	i++;
+	// }
 	if (pthread_create(&(table->death_monitor), NULL, death_monitor, table))
 	{
 		free(table->forks);
